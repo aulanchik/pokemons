aed43942f82887e10ad79e759295af2b2b85df13
Artyom Ulanchik <artyom.u@outlook.com>
2024-04-19 04:08:37 +0100

    fixed input

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const SearchInput = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ type = "text", ...props }, forwardRef) => {
+  ({ className, type = "text", ...props }, forwardRef) => {
     const inputStyle = "flex h-10 w-full border rounded-md px-4 py-2 text-lg";
     const textStyle = "text-black";
 
@@ -11,11 +11,11 @@ const SearchInput = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         ref={forwardRef}
-        className={`${inputStyle} ${textStyle}`}
+        className={`${inputStyle} ${textStyle} ${className}`}
         {...props}
       />
     );
-  }
+  },
 );
 
 export default SearchInput;
