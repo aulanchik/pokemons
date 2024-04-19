95270ba364c0f603bb08b303630fa86619a9c3ec
Artyom Ulanchik <artyom.u@outlook.com>
2024-04-19 05:36:46 +0100

    fixed types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,26 @@ interface Pokemon {
     id: string;
     name: string;
     image: string;
+    types: {
+        slot: number;
+        type: {
+            name: string;
+        };
+    }[];
+    stats: {
+        base_stat: number;
+        stat: {
+            name: string;
+        };
+    }[];
+    abilities: {
+        ability: {
+            name: string;
+        };
+    }[];
+    sprites: {
+        front_default: string;
+    };
 }
 
 export type { Pokemon };
