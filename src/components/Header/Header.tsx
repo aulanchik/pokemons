d211dbe6d99c005e2e464f8e87a2f878c329b3ec
Artyom Ulanchik <artyom.u@outlook.com>
2024-04-19 04:58:29 +0100

    Header added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
+
+const Header: React.FC = () => (
+    <div className="absolute top-0 left-0 m-4">
+        <Link href="/" className="text-blue-500 dark:text-blue-300 dark:hover:text-blue-100 hover:underline">
+            <div className="flex items-center gap-4">
+                <ArrowLeftIcon className="h-6 w-6" />
+                <span>Back to Pokemons</span>
+            </div>
+        </Link>
+    </div>
+);
+
+export default Header;
