3b30a9ee2ac152aa650e100c4c8eb2736399cea8
Artyom Ulanchik <artyom.u@outlook.com>
2024-04-21 23:59:43 +0100

    grid refactor

--- src/components/PokemonGrid/PokemonGrid.tsx
+++ src/components/PokemonGrid/PokemonGrid.tsx
@@ -15,26 +15,23 @@ const PokemonGrid: React.FC<PokemonGridProps> = ({ pokemons }): JSX.Element => {
         pokemon.name.toLowerCase().includes(query.toLowerCase())
     );
 
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(event.target.value);
+    };
+
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 p-4">
-            <div className="lg:col-span-4 flex flex-col items-center">
-                <h2 className="text-3xl font-bold text-black dark:text-white mt-8">Pokemon Collection</h2>
+        <div className="w-full py-6 px-4 md:px-6">
+            <div className="container mx-auto md:my-4 flex flex-col md:flex-row place-items-center md:items-center justify-around">
+                <h1 className="text-3xl font-bold text-black dark:text-white md:text-4xl lg:text-5xl">PoKeMoNs</h1>
+                <div className="relative w-full max-w-md md:ml-4">
+                    <SearchInput placeholder="Search for pokemons..." onChange={handleSearch} value={query} />
+                </div>
             </div>
 
             <div className="lg:col-span-4 flex flex-col items-center">
-                <SearchInput
-                    type="text"
-                    placeholder="Search for pokemons..."
-                    className="w-full lg:w-3/4 px-4 py-2 border rounded-md focus:outline-none"
-                    onChange={(e) => setQuery(e.target.value)}
-                    value={query}
-                />
-            </div>
-
-            <div className="lg:col-span-4 flex flex-col items-center">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
-                    {filteredPokemons.map((pokemon, index) => (
-                        <PokemonCard key={index} name={pokemon.name} image={pokemon.image} />
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+                    {filteredPokemons.map((pokemon: Pokemon) => (
+                        <PokemonCard key={pokemon.id} name={pokemon.name} image={pokemon.image} types={pokemon.types} />
                     ))}
                 </div>
             </div>
