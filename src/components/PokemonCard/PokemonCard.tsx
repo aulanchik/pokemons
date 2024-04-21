084c37f86538ad653c319f9ae7ae31dc99f6197d
Artyom Ulanchik <artyom.u@outlook.com>
2024-04-21 23:59:19 +0100

    pokemon card refactor

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,23 +1,41 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { capitalize } from "@/utils";
+import { capitalize, removeHyphens } from "@/utils";
+import { PokemonType } from "@/types";
 
 interface PokemonCardProps {
     name: string;
     image: string;
+    types: PokemonType[];
 }
 
-const PokemonCard: React.FC<PokemonCardProps> = ({ name, image }: PokemonCardProps): JSX.Element => {
+const PokemonCard: React.FC<PokemonCardProps> = ({ name, image, types }: PokemonCardProps): JSX.Element => {
     return (
         <Link
             href={name}
-            className="group rounded-lg border border-gray-300 bg-white hover:shadow-2xl m-4 px-5 py-4 dark:bg-gray-800 dark:border-gray-700 dark:hover:shadow-indigo-300"
+            className={`group rounded-lg flex border-1 border-gray-300 bg-white dark:bg-gray-800 shadow-lg m-4 dark:border-gray-700 dark:hover:shadow-indigo-300 p-4`}
         >
-            <Image src={image} width={200} height={200} alt={name} />
-            <h1 className="text-black text-[24px] text-center font-bold dark:text-white mt-auto mb-auto group-hover:underline">
-                {capitalize(name)}
-            </h1>
+            <div className="flex flex-col w-full h-full items-start content-center">
+                <p className="text-black text-2xl leading-loose font-bold dark:text-white group-hover:underline">
+                    {capitalize(name)}
+                </p>
+                <div className="flex items-start content-center gap-2">
+                    {types.map((type: PokemonType) => (
+                        <div
+                            key={type["slot"]}
+                            className="border rounded-full bg-white dark:bg-gray-700 dark:border-gray-700"
+                        >
+                            <span className="text-sm leading-loose font-semibold text-black dark:text-white p-2">
+                                {removeHyphens(capitalize(type.type["name"]))}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="w-auto h-auto">
+                <Image src={image} width={150} height={150} className="contain-strict" alt={name} />
+            </div>
         </Link>
     );
 };
