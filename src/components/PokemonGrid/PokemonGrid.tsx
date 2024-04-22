eb9d79037a818aa15c33f59820f3d8dcfcc7a8d4
Artyom Ulanchik <artyom.u@outlook.com>
2024-04-22 02:58:08 +0100

    removing dark mode

--- src/components/PokemonGrid/PokemonGrid.tsx
+++ src/components/PokemonGrid/PokemonGrid.tsx
@@ -22,7 +22,7 @@ const PokemonGrid: React.FC<PokemonGridProps> = ({ pokemons }): JSX.Element => {
     return (
         <div className="w-full py-6 px-4 md:px-6">
             <div className="container mx-auto md:my-4 flex flex-col md:flex-row place-items-center md:items-center justify-around">
-                <h1 className="text-3xl font-bold text-black dark:text-white md:text-4xl lg:text-5xl">PokeDeX Wiki</h1>
+                <h1 className="text-3xl font-bold text-black md:text-4xl lg:text-5xl">PokeDeX Wiki</h1>
                 <div className="relative w-full max-w-md md:ml-4">
                     <SearchInput placeholder="Search for pokemons..." onChange={handleSearch} value={query} />
                 </div>
