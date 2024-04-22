879b742fb4281ab771b575b2499d7223f3dd77d7
Artyom Ulanchik <artyom.u@outlook.com>
2024-04-22 03:55:51 +0100

    pokemon card appearances update

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { capitalize, removeHyphens } from "@/utils";
 import { PokemonType } from "@/types";
+import useColorData from "@/hooks";
 
 interface PokemonCardProps {
     name: string;
@@ -11,22 +12,35 @@ interface PokemonCardProps {
 }
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ name, image, types }: PokemonCardProps): JSX.Element => {
+    const [backgroundColor, setBackgroundColor] = useState("");
+    const [palette, setPalette] = useState<string[]>([]);
+    const { paletteColors, predominantColor } = useColorData(image);
+
+    useLayoutEffect(() => {
+        if (predominantColor) setBackgroundColor(predominantColor);
+        if (paletteColors) setPalette(paletteColors);
+    }, [predominantColor, palette]);
+
     return (
         <Link
             href={name}
-            className={`group rounded-lg flex border-1 border-gray-300 bg-white dark:bg-gray-800 shadow-lg m-4 dark:border-gray-700 dark:hover:shadow-indigo-300 p-4`}
+            className={`group rounded-lg flex flex-row border-gray-300 shadow-xl transition 0.125s ease-in m-4 p-4`}
+            style={{
+                background: `${backgroundColor}`,
+                opacity: backgroundColor ? 1 : 0,
+                border: `1px solid ${backgroundColor}`
+            }}
         >
             <div className="flex flex-col w-full h-full items-start content-center">
-                <p className="text-black text-2xl leading-loose font-bold dark:text-white group-hover:underline">
-                    {capitalize(name)}
-                </p>
+                <p className="text-white text-3xl leading-loose font-bold group-hover:underline">{capitalize(name)}</p>
                 <div className="flex items-start content-center gap-2">
                     {types.map((type: PokemonType) => (
                         <div
                             key={type["slot"]}
-                            className="border rounded-full bg-white dark:bg-gray-700 dark:border-gray-700"
+                            style={{ border: `1px solid ${palette[3]}` }}
+                            className="border rounded-full bg-white bg-opacity-80"
                         >
-                            <span className="text-sm leading-loose font-semibold text-black dark:text-white p-2">
+                            <span className="text-md text-black leading-loose p-2">
                                 {removeHyphens(capitalize(type.type["name"]))}
                             </span>
                         </div>
