102a943688b25fe69118e6e9993995bf9e34d7df
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-17 19:43:25 +0100

    added removeDuplicates util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { default as debounce } from "./debounce";
 export { default as capitalize } from "./capitalize";
 export { default as formatEntry } from "./formatEntry";
 export { default as removeHyphens } from "./removeHyphens";
+export { default as removeDuplicates } from "./removeDuplicates";
