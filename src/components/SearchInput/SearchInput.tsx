160489afadc9bbad51333c13e0c2029d415692bc
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-17 21:08:33 +0100

    fixed magnifier appearance on search input

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -4,14 +4,15 @@ import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const SearchInput = React.forwardRef<HTMLInputElement, InputProps>(({ type = "text", ...props }, forwardRef) => {
+    const inputContainerStyle = "relative w-full";
     const inputStyle =
-        "w-full rounded-md px-4 py-2 border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500";
-    const textStyle = "text-black";
+        "w-full rounded-md px-10 py-2 border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500";
+    const iconStyle = "absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400";
 
     return (
-        <div>
-            <input type={type} ref={forwardRef} className={`${inputStyle} ${textStyle}`} {...props} />
-            <MagnifyingGlassIcon className="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
+        <div className={inputContainerStyle}>
+            <input type={type} ref={forwardRef} className={inputStyle} {...props} />
+            <MagnifyingGlassIcon className={iconStyle} />
         </div>
     );
 });
