df18c5e6fba2110f394199c0a4c2198ba02eeaae
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-17 19:42:28 +0100

    added debounce util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export { default as debounce } from "./debounce";
 export { default as capitalize } from "./capitalize";
 export { default as formatEntry } from "./formatEntry";
 export { default as removeHyphens } from "./removeHyphens";
