8167f6b74419bf3934c995996869db8150b0e642
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-17 14:49:20 +0100

    refactor PokemonGrid

--- src/components/PokemonGrid/PokemonGrid.tsx
+++ src/components/PokemonGrid/PokemonGrid.tsx
@@ -1,42 +1,32 @@
 "use client";
 
-import React, { useState } from "react";
-import { PokemonCard, SearchInput } from "@/components";
+import React, { useCallback, useState } from "react";
+import { PokemonList, SearchBar } from "@/components";
+import { removeDuplicates, debounce } from "@/utils";
 import { Pokemon } from "@/types";
 
 interface PokemonGridProps {
     pokemons: Pokemon[];
 }
 
-const removeDuplicates = (pokemons: Pokemon[]) =>
-    pokemons.filter((pokemon, index) => pokemons.findIndex((otherPokemon) => otherPokemon.id === pokemon.id) === index);
-
 const PokemonGrid: React.FC<PokemonGridProps> = ({ pokemons }): JSX.Element => {
     const [query, setQuery] = useState<string>("");
 
-    const filteredPokemons = removeDuplicates(pokemons).filter((pokemon: Pokemon) =>
+    const uniquePokemons = removeDuplicates(pokemons, (pokemon: Pokemon) => pokemon.id);
+
+    const filteredPokemons = uniquePokemons.filter((pokemon: Pokemon) =>
         pokemon.name.toLowerCase().includes(query.toLowerCase())
     );
 
-    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setQuery(event.target.value);
-    };
+    const handleSearch = useCallback(
+        debounce((searchQuery: string) => setQuery(searchQuery), 500),
+        []
+    );
 
     return (
-        <div className="w-full py-6 md:px-6">
-            <div className="container mx-auto md:my-4 flex flex-col md:flex-row place-items-center justify-around md:items-center space-y-2">
-                <h1 className="text-3xl font-bold text-black md:text-4xl lg:text-5xl">PokeDeX Wiki</h1>
-                <div className="relative w-full max-w-md md:ml-4">
-                    <SearchInput placeholder="Search for pokemons..." onChange={handleSearch} value={query} />
-                </div>
-            </div>
-            <div className="lg:col-span-4 flex flex-col items-center">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                    {filteredPokemons.map((pokemon: Pokemon) => (
-                        <PokemonCard key={pokemon.id} {...pokemon} />
-                    ))}
-                </div>
-            </div>
+        <div className="h-full md:px-6 py-6">
+            <SearchBar query={query} onQuery={handleSearch} />
+            <PokemonList pokemons={filteredPokemons} />
         </div>
     );
 };
