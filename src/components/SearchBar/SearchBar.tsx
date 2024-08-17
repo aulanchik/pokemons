28add1bb930ac3fc1f06807f8f047099a9671b60
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-17 16:42:38 +0100

    adjust appearance of searchbar with logo

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -12,8 +12,8 @@ const SearchBar: React.FC<SearchBarProps> = ({ query, onQuery }): JSX.Element =>
     };
 
     return (
-        <div className="w-full mx-auto px-4 py-6">
-            <div className="flex items-center justify-between">
+        <div className="w-full px-4 py-6">
+            <div className="flex flex-col md:flex-row items-center justify-between">
                 <h1 className="text-3xl font-bold text-black md:text-4xl lg:text-5xl">PokeDeX Wiki</h1>
                 <SearchInput placeholder="Search for pokemons..." onChange={handleSearch} value={query} />
             </div>
